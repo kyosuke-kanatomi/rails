ddc9c7b332d483531a8884bc1523d33ae76b36dc
金富恭佑 <kanatomikyosuke@kyosuke.local>
2019-09-05 18:00:54 +0900

    finish3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd99167b280edf516cdf50271d7af5dff4c488336c6b8c7ff01c94a38f1ba0cead0d186fd0a83db6037b58a4c3b455d47cdf2ff8af2d0c87e9c50a82edc4d3378'
